abc5b837ed2fd1cd0e5b64b218d26c81e174165a
unknown <ebubeidika@gmail.com>
2024-01-01 22:40:36 +0100

    working on the tab bar

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './Screens/HomeScreen';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
